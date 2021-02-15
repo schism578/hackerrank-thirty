bba6ba70379b246203b57de47caad19a184652f2
Adrian Marquis <adrian.marquis78@gmail.com>
2021-02-15 12:25:17 -0500

    class vs inheritance added

---
+++ class-vs-instance.js
@@ -0,0 +1,25 @@
+function person(initialAge){
+    // Add some more code to run some checks on initialAge
+    let age = initialAge;
+    if(initialAge < 0) {
+        initialAge = 0;
+        console.log('Age is not valid, setting age to 0.');
+    }
+  this.amIOld=function(){
+   // Do some computations in here and print out the correct statement to the console
+   if(initialAge < 13) {
+       console.log('You are young.')
+   } else if(initialAge >= 13 && initialAge < 18) {
+       console.log('You are a teenager.')
+   } else {
+       console.log('You are old.')
+   }
+
+  };
+   this.yearPasses=function(){
+          // Increment the age of the person in here
+          initialAge = initialAge + 1;
+   };
+}
+
+console.log(person());
